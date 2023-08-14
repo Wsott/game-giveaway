95312833ae766ad7818646ddcaa5932fe428c696
Emiliano Gomez Olivera <theguiltyspark343@outlook.com>
2023-08-14 17:21:54 -0300

    Cambio de titulo y landing

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -3,16 +3,17 @@ import style from "../../css/Components.module.css";
 export default function Landing () {
     return (
         <div className={style.landingContainer}>
-            <p>
+            <div className={style.landingText}>
+                <p>
                 Esta pagina fue hecha para ver que juegos estan gratis en diferentes plataformas como 
                 Steam, Epic Games, GOG, itch.io, entre otras.
-            </p>
-            <br />
-            <p>¯\_(ツ)_/¯</p>
-            <br />
-            <p>
-                Principalmente esta pensada para Epic Games y Steam.
-            </p>
+                </p>
+                <br />
+                <p>
+                    Principalmente esta pensada para Epic Games y Steam.
+                </p>
+            </div>
+            <img className={style.landingImage} src="icon.png" alt="Un gran trato" />
         </div>
     );
 }
