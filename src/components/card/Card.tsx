864f870db694453ecbfa0f712653949460880377
Emiliano Gomez Olivera <theguiltyspark343@outlook.com>
2023-08-14 14:57:31 -0300

    Agregado fetching a la API

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,4 +1,6 @@
+import { useQuery } from "react-query";
 import style from "../../css/Components.module.css";
+import axios from "axios";
 
 const mockData = {
     "id": 1155,
@@ -18,18 +20,51 @@ const mockData = {
     "gamerpower_url": "https://www.gamerpower.com/europa-universalis-iv"
 }
 
-export default function Card() {
+// async function fetchData (plataforma: string) {
+//     const options = {
+//         method: 'GET',
+//         url: 'https://gamerpower.p.rapidapi.com/api/giveaways?platform=' + plataforma,
+//         headers: {
+//             'X-RapidAPI-Key': 'b07370299dmsh4b98a0bfd7af1a8p17260ajsn7018346b3344',
+//             'X-RapidAPI-Host': 'gamerpower.p.rapidapi.com'
+//         }
+//     };
+//     try {
+//         const response = await axios.request(options);
+//         console.log(response.data);
+//         return response.data;
+//     } catch (error) {
+//         console.error(error);
+//     }
+//     /*const res = await fetch("https://www.gamerpower.com/api/giveaways?platform=" + plataforma, {mode: "no-cors"});
+//     if (!res.ok) {
+//         console.log(res);
+//         throw new Error ("ERROR");
+//     }*/
+//     //return res.json();
+// }
+
+export interface cardData {
+    image: string;
+    title: string;
+    type: string;
+    worth: string;
+    end_date: string;
+    platforms: string;
+}
+
+export default function Card({image, title, type, worth, end_date, platforms}: cardData) {
     return (
         <div className={style.cardContainer}>
             <div className={style.cardImageContainer}>
-                <img className={style.cardImage} src={mockData.thumbnail} alt={mockData.title} />
+                <img className={style.cardImage} src={image} alt={"Image of " + title} />
             </div>
             <div className={style.cardBodyContainer}>
-                <p className={style.title}>{mockData.title}</p>
-                <p><b>Tipo:</b> {(mockData.type == "Game")? "Juego": mockData.type}</p>
-                <p><b>Vale:</b> <span className={style.crossedText}>{mockData.worth}</span></p>
-                <p><b>Disponible hasta:</b> {mockData.end_date.split(" ")[0]}</p>
-                <p><b>Tags:</b> <i>{mockData.platforms}</i></p>
+                <p className={style.title}>{title}</p>
+                <p><b>Tipo:</b> {(type == "Game")? "Juego": type}</p>
+                <p><b>Vale:</b> <span className={style.crossedText}>{worth}</span></p>
+                <p><b>Disponible hasta:</b> {end_date.split(" ")[0]}</p>
+                <p><b>Tags:</b> <i>{platforms}</i></p>
             </div>
         </div>
     );
--- src/components/grid/Grid.tsx
+++ src/components/grid/Grid.tsx
@@ -1,14 +1,66 @@
+import axios from "axios";
 import style from "../../css/Components.module.css";
-import Card from "../card/Card";
+import Card, { cardData } from "../card/Card";
+import { useQuery } from "react-query";
+
+interface jsonData {
+    "id": number;
+    "title": string;
+    "worth": string;
+    "thumbnail": string;
+    "image": string;
+    "description": string;
+    "instructions": string;
+    "open_giveaway_url": string;
+    "published_date": string;
+    "type": string;
+    "platforms": string;
+    "end_date": string;
+    "users": number;
+    "status": string;
+    "gamerpower_url": string;
+    "open_giveaway": string;
+}
+
+async function fetchData (plataforma: string) {
+    const options = {
+        method: 'GET',
+        url: 'https://gamerpower.p.rapidapi.com/api/giveaways?platform=' + plataforma,
+        headers: {
+            'X-RapidAPI-Key': 'b07370299dmsh4b98a0bfd7af1a8p17260ajsn7018346b3344',
+            'X-RapidAPI-Host': 'gamerpower.p.rapidapi.com'
+        }
+    };
+    try {
+        const response = await axios.request(options);
+        console.log(response.data);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 export default function Grid () {
+    const {data, isLoading, error} = useQuery("data", () => fetchData("epic-games-store"));
+
     return (
         <div className={style.gridContainer}>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
+            {
+                (isLoading)?
+                    null
+                :
+                    data.map((actual: jsonData) => {
+                        return (
+                            <Card 
+                                image={actual.thumbnail} 
+                                title={actual.title} 
+                                type={actual.type} 
+                                worth={actual.worth} 
+                                end_date={actual.end_date} 
+                                platforms={actual.platforms}/>
+                        )
+                    })    
+            }
         </div>
     );
 }
